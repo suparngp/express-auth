a1bf133b1aef2dd31876123ac6bfc0126b14c159
Suparn Gupta <gupta.suparn@gmail.com>
2014-01-12 01:10:40 -0600

    Version 0.0.1

---
+++ express-auth.js
@@ -0,0 +1,78 @@
+/**
+ * Created by suparngupta on 1/11/14.
+ */
+
+/**
+ * This module creates a store for the express application
+ * to manage the sessions using headers instead of cookies.
+ * This is helpful in cases where the client is not a web browser
+ * or OAuth like functionality is required.
+ * To every request, a header is attached whose name can be configured.
+ * The client is required to add this header to the request.
+ * Upon receiving the req, the header is checked for the value.
+ * If the given header value is not present in the database, next(err) is called.
+ * Other wise if everything is ok, next() is called.
+ * A custom err object can also be defined. By default the application uses the
+ * Error Object with message "Unauthorized".
+ * To Delete a session, a function req.authorize.destroy() can be called.
+ * The current session information is stored in the object req.authorize object.
+ */
+
+var Authorize = require('./lib/Authorize'),
+    AuthStore = require('./lib/AuthStore');
+
+var defaultConfig = {
+    secret: '',
+    header: 'authorization',
+    exclude: [],
+    error: new Error('Unauthorized'),
+    store: {
+        db: 'test',
+        host: 'localhost',
+        port: '27017',
+        username: '',
+        password: '',
+        collection: 'session',
+        w: 1,
+        expiration: 7200
+    }
+};
+
+exports.setup = function (options, callback) {
+    var storeOptions = options.store;
+    if(storeOptions){
+        for(var y in storeOptions){
+            if(storeOptions.hasOwnProperty(y)){
+                defaultConfig.store[y] = storeOptions[y];
+            }
+        }
+    }
+    options.store = defaultConfig.store;
+    for(var x in defaultConfig){
+        if(defaultConfig.hasOwnProperty(x) && !options[x]){
+            options[x] = defaultConfig[x];
+        }
+    }
+
+    var authStore = new AuthStore(options.store, callback);
+
+    function middleware(req, res, next) {
+
+        req.authorize = new Authorize(req, res, {
+            header: options.header,
+            secret: options.secret,
+            store: authStore
+        });
+
+        if (options.exclude.indexOf(req.path) !== -1) {
+            next();
+            return;
+        }
+
+        req.authorize.get(function(err, session){
+            if(err || !session) next(options.error);
+            else next();
+        });
+    }
+    return middleware;
+};
---
+++ test/test.js
@@ -0,0 +1,186 @@
+/**
+ * Created by suparngupta on 1/11/14.
+ */
+
+var expect = require('expect.js'),
+    expressAuth = require('./../express-auth'),
+    _ = require('underscore');
+
+var config = {
+    secret: 'someone',
+    header: 'custom-header',
+    exclude: ['/login'],
+    error: new Error('Custom error object'),
+    store: {
+        db: 'quix',
+        host: 'localhost',
+        port: '27017',
+        username: 'quix',
+        password: 'quix@123',
+        collection: 'session',
+        w: 1,
+        expiration: 7200
+    }
+};
+describe('Tests for express auth', function () {
+    describe('create session', function () {
+
+
+        it('should not check for session- login path', function (done) {
+            var req = {
+                get: function (header) {
+                    return '';
+                },
+                path: '/login'
+            };
+            var res = {
+                header: '',
+                set: function (header, value) {
+                    expect(header).to.be(config.header);
+                    expect(!value).to.be(false);
+                    res.header = value;
+                }
+            };
+            var middleware = expressAuth.setup(config, function (err) {
+                expect(!err).to.be.ok();
+                middleware(req, res, function (err) {
+                    expect(!err).to.be.ok();
+                    req.authorize.createSession('1234', function (err, session) {
+                        expect(!err).to.be.ok();
+                        expect(!session).not.to.be.ok();
+                        expect(!session._id).not.to.be.ok();
+                        expect(session.id).to.be(session._id);
+                        expect(_.isEqual(req.authorize.session, session)).to.be.ok();
+                        expect(res.header).to.be(session.id + ':' + session.token + ':' + session.signature);
+                        done();
+                    })
+
+                });
+
+            });
+
+
+        });
+        it('should check for session', function (done) {
+            var req = {
+                get: function (header) {
+                    return '';
+                },
+                path: '/login'
+            };
+            var res = {
+                header: '',
+                set: function (header, value) {
+                    expect(header).to.be(config.header);
+                    expect(!value).to.be(false);
+                    res.header = value;
+                    req.get = function(header){
+                        return value;
+                    }
+                }
+            };
+            var middleware = expressAuth.setup(config, function (err) {
+                expect(!err).to.be.ok();
+                middleware(req, res, function (err) {
+                    expect(!err).to.be.ok();
+                    req.authorize.createSession('1234', function (err, session) {
+                        req.path = '';
+                        middleware(req, res, function(err){
+                            expect(!err).to.be.ok();
+                            done();
+                        });
+                    })
+
+                });
+
+            });
+
+
+        });
+
+        it('should check for session timeouts- 3secs', function (done) {
+            done();
+            return;
+            this.timeout(80000);
+            var req = {
+                get: function (header) {
+                    return '';
+                },
+                path: '/login'
+            };
+            var res = {
+                header: '',
+                set: function (header, value) {
+                    expect(header).to.be(config.header);
+                    expect(!value).to.be(false);
+                    res.header = value;
+                    req.get = function(header){
+                        return value;
+                    }
+                }
+            };
+            config.store.expiration = 3;
+            var middleware = expressAuth.setup(config, function (err) {
+                expect(!err).to.be.ok();
+                middleware(req, res, function (err) {
+                    expect(!err).to.be.ok();
+                    req.authorize.createSession('1234', function (err, session) {
+                        req.path = '';
+                        middleware(req, res, function(err){
+                            expect(!err).to.be.ok();
+                            setTimeout(function(){
+
+                                middleware(req, res, function(err){
+                                    config.store.expiration = 7200;
+                                    expect(!err).to.be(false);
+                                    done();
+                                });
+                            }, 60000);
+
+                        });
+                    })
+
+                });
+
+            });
+
+
+        });
+
+        it('should destroy a session', function (done) {
+            var req = {
+                get: function (header) {
+                    return '';
+                },
+                path: '/login'
+            };
+            var res = {
+                header: '',
+                set: function (header, value) {
+                    expect(header).to.be(config.header);
+                    res.header = value;
+                    req.get = function(header){
+                        return value;
+                    }
+                }
+            };
+            var middleware = expressAuth.setup(config, function (err) {
+                expect(!err).to.be.ok();
+                middleware(req, res, function (err) {
+                    expect(!err).to.be.ok();
+                    req.authorize.createSession('1234', function (err, session) {
+                        req.path = '';
+                        middleware(req, res, function(err){
+                            expect(!err).to.be.ok();
+                            req.authorize.destroySession(function(err, session){
+                                console.log(err);
+                                expect(!err).to.be.ok();
+                                done();
+                            });
+                        });
+                    })
+                });
+            });
+        });
+    });
+});
